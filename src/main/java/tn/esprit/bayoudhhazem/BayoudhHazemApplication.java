0faa243752a9252edfa5e648b903ee36ac5e8343
BHazem <98537270+BHazem@users.noreply.github.com>
2022-11-23 09:15:58 +0100

    FirstCommit

---
+++ src/main/java/tn/esprit/bayoudhhazem/BayoudhHazemApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.bayoudhhazem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BayoudhHazemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BayoudhHazemApplication.class, args);
+    }
+
+}
